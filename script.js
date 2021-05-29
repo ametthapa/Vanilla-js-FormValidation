d2a7eab59dbd92a4b4b1281fffe2389c3fca93db
Amrit Thapa <amet.thapa27@gmail.com>
2021-05-29 09:35:59 +0545

    script.js
    Password validation

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function checkPassword(input){
     if(re.test(input.value)){
         showSuccess(input);
     }else{
-        showError(input,' contain a number, lowercase & uppercase character');
+        showError(input,' contain a number, lowercase & uppercase');
     }
 }
 
